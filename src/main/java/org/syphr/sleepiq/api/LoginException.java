3838400fe63cc58730c6a22dd3538bb6dece5012
Gregory Moyer <moyerg@syphr.com>
2017-02-18 01:13:22 -0500

    Refactored LoginException to use the new parent exception

--- src/main/java/org/syphr/sleepiq/api/LoginException.java
+++ src/main/java/org/syphr/sleepiq/api/LoginException.java
@@ -1,33 +1,28 @@
+/*
+ * Copyright 2017 Gregory Moyer
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.syphr.sleepiq.api;
 
 import org.syphr.sleepiq.api.model.Failure;
 
-public class LoginException extends Exception
+public class LoginException extends SleepIQException
 {
     private static final long serialVersionUID = 1L;
 
-    public LoginException()
-    {
-        super();
-    }
-
-    public LoginException(String message, Throwable cause)
-    {
-        super(message, cause);
-    }
-
-    public LoginException(String message)
-    {
-        super(message);
-    }
-
-    public LoginException(Throwable cause)
-    {
-        super(cause);
-    }
-
     public LoginException(Failure failure)
     {
-        this("Login failed: " + failure.getError().getMessage());
+        super(failure);
     }
 }
